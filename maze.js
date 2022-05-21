7dbdd5cd0f34008426b40e04d50a15b1800c80b5
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-05-21 22:33:56 +0300

    feat - border color, reset fn

--- maze.js
+++ maze.js
@@ -21,4 +21,39 @@ window.onload = function () {
     colorBoundaries("#564a4a");
     borderCheck();
   }
+
+  function borderCheck() {
+    document.addEventListener("mousemove", function (e) {
+      // if cursor touches a div with boundary class user loses
+      const cursor = e.target.classList.value;
+      if (cursor == "boundary" && game_state == "active") {
+        colorBoundaries("#cd4439");
+        score -= 10;
+        status.innerHTML = `You lost<br> Score: ${score}`;
+        game_state = "lost";
+      }
+    });
+  }
+
+  function win() {
+    if (game_state != "lost" && game_state != "start") {
+      colorBoundaries("#4aa96c");
+      score += 5;
+      status.innerHTML = `You won<br> Score: ${score}`;
+      game_state = "start";
+    }
+  }
+
+  // div colors
+  function colorBoundaries(color) {
+    for (let i = 0; i < maze_borders.length; i++) {
+      maze_borders[i].style.background = color;
+    }
+  }
+
+  // restart game
+  function reset() {
+    score = 0;
+    status.innerHTML = "Game restarted";
+  }
 };
